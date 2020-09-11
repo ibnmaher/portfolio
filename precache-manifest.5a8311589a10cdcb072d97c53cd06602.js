61c8d43015085ce07cf5df90583694920c78d103
Abdulrahman Maher <ibn.maher.96@gmail.com>
2020-09-11 14:15:10 +0300

    Updates

---
+++ precache-manifest.5a8311589a10cdcb072d97c53cd06602.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a671f51a630c13c645c075afb9a2a830",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "07f6511d38edd87aa549",
+    "url": "/my-app/static/css/main.83555537.chunk.css"
+  },
+  {
+    "revision": "3d88a4992fa225aa14ea",
+    "url": "/my-app/static/js/2.f3c3c3df.chunk.js"
+  },
+  {
+    "revision": "4f4a12233651b0855fd74d4d32936871",
+    "url": "/my-app/static/js/2.f3c3c3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f6511d38edd87aa549",
+    "url": "/my-app/static/js/main.9c204000.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "f54294e3d7b47146e57249709068a6b9",
+    "url": "/my-app/static/media/2.f54294e3.jpg"
+  },
+  {
+    "revision": "39119b264ee48a77a5e7a39fe1f40582",
+    "url": "/my-app/static/media/background.39119b26.jpg"
+  }
+]);
