1020e35f46b6ca847993c328f75beba9abf4d2c0
Abdulrahman Maher <ibn.maher.96@gmail.com>
2020-09-13 11:34:24 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b34e6b68ff252d9faf462334292aef26.js"
+  "/portfolio/precache-manifest.5013e2267ea994d34e161597cf23b0f1.js"
 );
 
 self.addEventListener('message', (event) => {
