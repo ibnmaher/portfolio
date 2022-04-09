67efe52b39601bb8380622b580c9fc614a6f0701
Mohammed Maher <mm91ed@gmail.com>
2022-04-10 02:52:51 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f49d6b68c2b41f2b10e341ade6118067.js"
+  "/portfolio/precache-manifest.6ef2a83887fe30fa64601520e3ab1b76.js"
 );
 
 self.addEventListener('message', (event) => {
